25872dd30fab4b63a80703309055d8a28cce7aa3
GoodWin <good.win.alexs@gmail.com>
2016-01-28 16:20:31 +0200

    Update headers

--- LightTelemetry.h
+++ LightTelemetry.h
@@ -1,3 +1,16 @@
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  A copy of the GNU General Public License is available at <http://www.gnu.org/licenses/>.
+//
+
 #define LIGHTTELEMETRY_START1 0x24 //$
 #define LIGHTTELEMETRY_START2 0x54 //T
 #define LIGHTTELEMETRY_GFRAME 0x47 //G GPS + Baro altitude data ( Lat, Lon, Speed, Alt, Sats, Sat fix)
--- MavLink.cpp
+++ MavLink.cpp
@@ -1,3 +1,16 @@
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  A copy of the GNU General Public License is available at <http://www.gnu.org/licenses/>.
+//
+
 //#include "../mavlink/include/mavlink.h"
 //#include "./GCS_MAVLink/include/mavlink/v1.0/mavlink_types.h"
 //#include "./GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/mavlink.h"
